986d2bc9c101488b7e532e0c1afcec54bc1e60ad
aminadounia <ja_slimane@esi.dz>
2023-04-26 12:05:02 +0200

    menu added

--- app/src/main/java/com/example/onppe_v1/MainActivity.kt
+++ app/src/main/java/com/example/onppe_v1/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.onppe_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.NavController
 
+import androidx.navigation.ui.NavigationUI
+import com.example.onppe_v1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(1).isEnabled = false
+
+        val navHostFragment = supportFragmentManager. findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
+
     }
 }
