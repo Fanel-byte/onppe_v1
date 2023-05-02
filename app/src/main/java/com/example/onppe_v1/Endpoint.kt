67719c94f2e5de9c2f3f196fac5b67c0ca121719
fanel_byte <jf_bentaleb@esi.dz>
2023-05-02 20:13:36 +0100

    Version with backend Folders

---
+++ app/src/main/java/com/example/onppe_v1/Endpoint.kt
@@ -0,0 +1,9 @@
+package com.example.onppe_v1
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.*
+
+interface Endpoint {
+
+
+}
---
+++ app/src/main/java/com/example/onppe_v1/url.kt
@@ -0,0 +1,3 @@
+package com.example.onppe_v1
+
+const val url="https://5617-185-107-56-134.eu.ngrok.io/"
