4ccf1605b81a40c4cc769bc443348ee872089e55
ikramsouames <ji_souames@esi.dz>
2023-05-07 19:38:42 +0100

    vf signalement avec les preuves

---
+++ app/src/main/java/com/example/onppe_v1/Citoyen.kt
@@ -0,0 +1,11 @@
+package com.example.onppe_v1
+
+data class Citoyen(
+    val id :Int?,
+    val nom:String?,
+    val prenom :String?,
+    val sexe:String?,
+    val age :Int?,
+    val adresse:String?,
+    val tel:String?
+)
